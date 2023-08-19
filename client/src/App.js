84c69fa1d14c125678a112bff88a0354970ef112
Mohansai.M <mohansaim20@gmail.com>
2023-08-19 22:10:15 +0530

    Added Login and Signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -4,17 +4,21 @@ import NavBar from './features/NavBar';
 import {  BrowserRouter as Router,Route,Routes } from 'react-router-dom';
 import Book from './Book';
 import AddBook from './features/AddBook';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
 
 function App() 
 {
    return (
-     <div className='App'>
+     <div className="App">
        <Router>
          <NavBar></NavBar>
          <Routes>
            <Route path="/Books" element={<BookLanding />} />
            <Route path="/Books/:isbn" element={<Book />} />
            <Route path="/AddBook" element={<AddBook />} />
+           <Route path="/Login" element={<Login />} />
+           <Route path="/Register" element={<Register />} />
          </Routes>
        </Router>
      </div>
